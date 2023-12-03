d1b836890b11c1f93fcf3968592b46ba74865e7e
Harry Xu <92757553+HarryXu497@users.noreply.github.com>
2023-12-02 20:11:45 -0500

    fixed meta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,12 @@ import SearchBar from "@/lib/components/SearchBar/SearchBar";
 import styles from "./page.module.css";
 import ProductList from '../lib/components/ProductList/ProductList';
 import getProducts from "@/lib/database/getProducts";
-import Head from "next/head";
 
 export default async function Page({ searchParams: { q } }: { searchParams: { q: string }}) {
 	const products = await getProducts(q);
 
 	return (
 		<main className={styles.productList}>
-			<Head>
-				<title>Product List</title>
-				<meta property="og:title" content="Product List" key="title"/>
-				<meta name="description" content="A simple product list :)"/>
-			</Head>
 			<SearchBar/>
 			<ProductList products={products}/>
 		</main>
